00da20a34f715d9a18d8dcf07c5a0a6720fa4cbc
Rohan Kothakapu <rkothaka@gmail.com>
2023-09-30 20:31:41 -0400

    gateway and rabbit-mq

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -40,7 +40,7 @@ def login():
         return "invalid credentials", 401
 
 
-@server.route("/validate", method=["POST"])
+@server.route("/validate", methods=["POST"])
 def valdiate():
     encoded_jwt = request.headers["Authorization"]
 
